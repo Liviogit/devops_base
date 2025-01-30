8c3d5e67ece6ff12c37c3fa40bbe9c80c97e713d
Livio <livio.daninthe@edu.esiee.fr>
2025-01-30 20:45:00 +0000

    error

--- td5/scripts/tofu/live/ci-cd-permissions/main.tf
+++ td5/scripts/tofu/live/ci-cd-permissions/main.tf
@@ -3,14 +3,14 @@ provider "aws" {
 }
 
 module "oidc_provider" {
-  source = "github.com/Liviogit/devops_base//td5/scripts/tofu/modules/github-aws-oidc"
+  source = "github.com/devops_base/tree/opentofu-tests/td5/scripts/tofu/modules/github-aws-oidc"
 
   provider_url = "https://token.actions.githubusercontent.com" 
 
 }
 
 module "iam_roles" {
-  source = "github.com/Liviogit/devops_base//td5/scripts/tofu/modules/gh-actions-iam-roles"
+  source = "github.com/devops_base/tree/opentofu-tests/td5/scripts/tofu/modules/gh-actions-iam-roles"
 
   name              = "lambda-sample"                           
   oidc_provider_arn = module.oidc_provider.oidc_provider_arn    
