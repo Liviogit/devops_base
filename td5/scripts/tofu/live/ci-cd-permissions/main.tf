fb6a988105ecbcafdb5728d3f72a43c9f50b5be4
Livio <livio.daninthe@edu.esiee.fr>
2025-01-31 14:29:39 +0000

    error

--- td5/scripts/tofu/live/ci-cd-permissions/main.tf
+++ td5/scripts/tofu/live/ci-cd-permissions/main.tf
@@ -5,25 +5,25 @@ provider "aws" {
 module "oidc_provider" {
   source = "git::https://github.com/Liviogit/devops_base.git//td5/scripts/tofu/modules/github-aws-oidc?ref=opentofu-tests"
 
-  provider_url = "https://token.actions.githubusercontent.com" 
-
+  provider_url = "https://token.actions.githubusercontent.com"
 }
 
 module "iam_roles" {
-  source = "git::https://github.com/Liviogit/devops_base.git//td5/tofu/modules/gh-actions-iam-roles?ref=opentofu-tests"
+  source = "git::https://github.com/Liviogit/devops_base.git//td5/scripts/tofu/modules/gh-actions-iam-roles?ref=opentofu-tests"
+
   name              = "lambda-sample"                           
   oidc_provider_arn = module.oidc_provider.oidc_provider_arn    
 
   enable_iam_role_for_testing = true                            
 
-  # TODO: fill in your own repo name here!
+  # TODO: fill in your own repo name ici
   github_repo      = "Liviogit/devops_base" 
   lambda_base_name = "lambda-sample"                            
 
   enable_iam_role_for_plan  = true                                
   enable_iam_role_for_apply = true                                
 
-  # TODO: fill in your own bucket and table name here!
+  # TODO: fill in your own bucket and table name ici
   tofu_state_bucket         = "fundamentals-of-devops-tofu-state" 
   tofu_state_dynamodb_table = "fundamentals-of-devops-tofu-state" 
 }
